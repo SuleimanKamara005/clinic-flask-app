9274f4ce4c2571e260a52aca983996c47f2972dd
suleimanKamara05 <suleimansuleiman442@yahoo.com>
2026-03-19 15:29:28 +0100

    Implemented enqueue operation for adding patients

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def add_patient():
         patient = Patient(name)
         # update
         queue.append(patient)  # enqueue
-
+        # update
         return redirect("/")
-
+      
     return render_template("add.html")
 if __name__ == "__main__":
     app.run(debug=True)
